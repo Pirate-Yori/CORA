be42fe6d93d1bd3ed2f0455ab2f7ad9442e37461
Drizain <drissa.traore@epitech.eu>
2026-01-20 08:54:54 +0000

    [UPDATE]rectification de l'authlayer et de register layer pour les couleurs

--- cora-frontend/src/views/auth/RegisterView.vue
+++ cora-frontend/src/views/auth/RegisterView.vue
@@ -5,16 +5,15 @@ import { useRouter } from 'vue-router';
 const router = useRouter()
 
 const goToLogin = ()=>{
-    console.log('emmit declenché')
 router.push({name:"Login"})
 }
-const handleSuccess = ()=>{
-    router.push({name:"Home"})
+const goToHome = ()=>{
+    router.push({name:'Home'})
 }
 </script>
 
 <template>
     <div>
-        <RegisterForm @success="goToLogin" @login="goToLogin"/>
+        <RegisterForm @success="goToLogin" @login="goToLogin" @home="goToHome"/>
     </div>
 </template>
